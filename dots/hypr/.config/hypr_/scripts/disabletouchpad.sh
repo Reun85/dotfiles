d0c5b9de4f77901d7a4fcf67b0690eabfea7c9b1
Reun85 <gugigergo@gmail.com>
2024-04-14 13:55:14 +0200

    mostofhypr

--- dots/hypr/.config/hypr_/scripts/disabletouchpad.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-device_name=$(hyprctl devices | rg "touchpad" | awk '{$1=$1;printf("device:%s:enabled",$1)}')
-new_status=$(hyprctl getoption "$device_name" | awk -F': ' '/int/{if ($2 == "1") print "0"; else if ($2 == "0") print "1"}')
-hyprctl keyword "$device_name" "$new_status"
-if [[ $new_status == "1" ]]; then
-	notify-send "Touchpad has been enabled"
-else
-	notify-send "Touchpad has been disabled"
-fi
